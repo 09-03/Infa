e90701ef102d1fac3195260f9b25e2db950795df
09-03 <71666046+09-03@users.noreply.github.com>
2020-09-22 23:24:16 +0300

    Update Factorial.py

--- Factorial.py
+++ Factorial.py
@@ -1,15 +1,16 @@
+# Функция Факториала
 def P(n):
     if n == 0:
         return 1
     elif n < 0:
         return "Введено отрицательное число"
     return P(n-1) * n
-
+# Функция Размещений
 def A(n, k):
     if n >= k:
         return P(n) / P(n - k)
     return "Error: n должно быть больше k"
-
+# Функция Сочетаний
 def C(n,k):
     if n >= k:
         return A(n, k) / P(k)
