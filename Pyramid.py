9833c091bada264a8da6b3739e130bebd7258f7f
09-03 <71666046+09-03@users.noreply.github.com>
2020-09-22 22:27:23 +0300

    Add files via upload

---
+++ Pyramid.py
@@ -0,0 +1,45 @@
+def vivod():
+    for i in range(len(a)):
+        for j in range(len(a[i])):
+            print(a[i][j], end=' ')
+        print()
+
+
+def triangle(y):
+    z = 0
+    p = 0
+    q = 0
+    for x in range(y):
+        for i in range(0 + z, n - 1 - z):
+            a[i + 1][n - i + z - 2] = a[i][n - i + z - 1] + 1
+        z += 1
+        for i in range(0 + p, 2 * n - 2 - 3 * p):
+            a[n - 1 - p][i + 1 + p] = a[n - 1 - p][i + p] + 1
+        p += 1
+        for i in range(0 + q, n - 2 - q):
+            a[n - 2 - i][2 * n - 3 - i - q] = a[n - 1 - i][2 * n - 2 - i - q] + 1
+        q += 1
+        if (n != 1) and (n != 2):
+            a[n - 2 - i][n - 1] = a[n - 2 - i][n] + 1
+
+
+def zamena():
+    for i in range(len(a)):
+        for j in range(len(a[i])):
+            if a[i][j] == 0:
+                a[i][j] = " "
+
+
+start = input("Запустить программу? Чтобы продолжить введите 'y'; для выхода - любую клавишу: ")
+while start == "y":
+    n = int(input("Кол-во строк: "))
+    if n > 0:
+        a = [[0] * (2 * n - 1) for i in range(n)]
+        a[0][n - 1] = 1
+        triangle(n // 2)
+        zamena()
+        vivod()
+        start = input("Запустить программу еще раз? Чтобы продолжить введите 'y'; для выхода - любую клавишу: ")
+    else:
+        print("ERROR: Количество строк введено неправильно")
+        start = input("Запустить программу еще раз? Чтобы продолжить введите 'y'; для выхода - любую клавишу: ")
