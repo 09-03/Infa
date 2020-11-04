9ca7276ebb67983dd4c1aea6a3ef74e555a24f38
09-03 <71666046+09-03@users.noreply.github.com>
2020-11-04 18:45:13 +0300

    Update Geoboard v 2.py

--- University_Python/Geoboard v 2.py
+++ University_Python/Geoboard v 2.py
@@ -1,13 +1,51 @@
+from tkinter import *
 import random
+import math
 
-"""
-Если придумаете как реализовать триангуляцию точек на плоскости
-без этих библеотек, тогда отпишите мне
-"""
 
-from scipy.spatial import Delaunay
-import numpy as np
- 
+def GrahamScan(stick_list):
+    stick_list.sort()
+    L_upper = [stick_list[0], stick_list[1]]
+    for i in range(2,len(stick_list)):
+        L_upper.append(stick_list[i])
+        while len(L_upper) > 2 and not RightTurn(L_upper[-1],L_upper[-2],L_upper[-3]):
+            del L_upper[-2]
+            
+    L_lower = [stick_list[-1], stick_list[-2]]
+    for i in range(len(stick_list)-3,-1,-1):
+        L_lower.append(stick_list[i])
+        while len(L_lower) > 2 and not RightTurn(L_lower[-1],L_lower[-2],L_lower[-3]):
+            del L_lower[-2]
+            
+    del L_lower[0]
+    del L_lower[-1]
+    L = L_upper + L_lower
+    return L
+
+
+def RightTurn(p1, p2, p3):
+    if (p3[1]-p1[1])*(p2[0]-p1[0]) >= (p2[1]-p1[1])*(p3[0]-p1[0]):
+        return False
+    return True
+
+
+def convex_hull(stick_list):
+    convex_hull_area = 0
+    if len(stick_list) < 3:
+        return convex_hull_area
+    
+    points_set = GrahamScan(stick_list)
+    for n in range(len(points_set)):
+        if n == len(points_set)-1:
+            convex_hull_area += int((points_set[n][0]*points_set[0][1]-
+                                 points_set[n][1]*points_set[0][0])/2)
+            
+            return abs(convex_hull_area)
+        
+        else:
+            convex_hull_area += int((points_set[n][0]*points_set[n+1][1]-
+                                 points_set[n][1]*points_set[n+1][0])/2)
+
 
 def stick_placing(N):
     stick_list = []
@@ -20,25 +58,6 @@ def stick_placing(N):
     return stick_list
 
             
-def polygons(stick_list):
-    S = 0
-    polygon_area = 0
-    if len(stick_list)<3:
-        return S
-    points = np.array(stick_list)
-    try:
-        triangles = Delaunay(points)
-    except:
-        return S
-    for polygon in points[triangles.simplices]:
-        for n in range(len(polygon)):
-            if n == len(polygon)-1:
-                polygon_area += (polygon[n][0]*polygon[0][1]- polygon[n][1]*polygon[0][0])/2
-                S+=abs(polygon_area)
-                polygon_area = 0
-            else:
-                polygon_area += (polygon[n][0]*polygon[n+1][1] - polygon[n][1]*polygon[n+1][0])/2
-    return int(S)
 
 
 def E(N, num_tests):
@@ -47,15 +66,18 @@ def E(N, num_tests):
     area_list=[]
     for i in range(num_tests):
         sticks = stick_placing(N)
-        area_list.append(polygons(sticks))
+        area_list.append(convex_hull(sticks))
         if area_list[i] in area_counter:
             area_counter[area_list[i]] += 1
+            
         else:
             area_counter[area_list[i]] = 1
+            
         print(f"Геодосок сделано: {i+1}/{num_tests}")
 
     for key, value in area_counter.items():
         E+=key*value
+        
     E /= (num_tests * 10000)
     print(round(E, 5))
 
