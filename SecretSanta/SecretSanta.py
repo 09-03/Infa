415dd4f11c017e621b093fc4fadff3b53b1e9c75
09-03 <71666046+09-03@users.noreply.github.com>
2020-11-28 00:43:38 +0300

    Add files via upload

---
+++ SecretSanta/SecretSanta.py
@@ -0,0 +1,13 @@
+import random
+n = int(input("Введите количество участников: "))
+list = [[i+1, i+1] for i in range(n)]
+random.shuffle(list)
+temp = list[0][1]
+for i in range(n):
+    if i == n-1:
+        list[i][1] = temp
+    else:
+        list[i][1] = list[i+1][1]
+    file = open(f"SecretSanta{i+1}.txt", "w")
+    file.write(f"Ваш номер участника: {list[i][0]}"  + "\n" + f"Вы дарите подарок участнику под номером: {list[i][1]}" + "\n")
+    file.close()
