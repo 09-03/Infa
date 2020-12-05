5ab35de47ebc928c22619bfba9876e79ab39c7e1
09-03 <71666046+09-03@users.noreply.github.com>
2020-12-06 00:26:10 +0300

    Update and rename sum(2 solutions).py to sum(3 solutions).py

--- University_Python/sum(3 solutions).py
+++ University_Python/sum(3 solutions).py
@@ -11,16 +11,34 @@ from random import randint
 massive = [randint(0,10) for i in range(20)]
 c = int(input("Заданное число C: "))
 
-
-def s_code(massive, c): # Итого O(n) + O(n^2) = O(n^2)
+def d_code(massive, c): # Итого O(n) + O(n) + O(n) = O(n)
     massive = list(set(massive)) # O(n)
-    for i in range(len(massive)):
+    num_dict = {}
+    for num in massive: # O(n)
+        if num not in num_dict:
+            num_dict[num] = num
+
+    for num in massive: # O(n)
+        if c-num in num_dict:
+            print(f"{num} + {num_dict[c-num]} = {c}")
+            del num_dict[num]
+
+
+def s_code(massive, c): # Итого O(n) + O(n) + O(n^2) = O(n^2)
+    if c % 2 == 0 and massive.count(c/2) > 1: # O(n)
+        print(f"{int(c/2)} + {int(c/2)} = {c}")
+
+    massive = list(set(massive)) # O(n)
+    for i in range(len(massive)):  # O(n^2) (Если быть точным, то O(0.5n^2 - n/2))
         for j in range(1, len(massive)-i):
-            if massive[i] + massive[j+i] == c: # O(n^2) (Если быть точным, то O(0.5n^2 - n/2))
+            if massive[i] + massive[j+i] == c:
                 print(f"{massive[i]} + {massive[j+i]} = {c}")
 
 
-def e_code(massive, c): # Итого O(n) + O(nlogn) + O(n) = O(nlogn)
+def e_code(massive, c): # Итого O(n) + O(n) + O(nlogn) + O(n) = O(nlogn)
+    if c % 2 == 0 and massive.count(c/2) > 1: # O(n)
+        print(f"{int(c/2)} + {int(c/2)} = {c}")
+
     massive = list(set(massive)) # O(n)
     massive.sort() # O(nlogn)
     i = 0
@@ -39,4 +57,8 @@ def e_code(massive, c): # Итого O(n) + O(nlogn) + O(n) = O(nlogn)
 print(massive)
 s_code(massive, c)
 print("---------------")
+print(massive)
 e_code(massive, c)
+print("---------------")
+print(massive)
+d_code(massive, c)
